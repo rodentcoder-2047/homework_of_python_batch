b170f9fac0508590ea5b166cdde6dc49fea0b4cc
rodentcoder-2047 <rodentcoder2047@gmail.com>
2025-11-06 13:31:42 +0530

    case conversion

---
+++ class 3 homework/Case conversion.py
@@ -0,0 +1,8 @@
+#Programme for case conversion
+Sentence = input(">")
+print("upper case")
+print(Sentence.upper())
+print ("lower case")
+print(Sentence.lower())
+print("Tittle")
+print(Sentence.title())
